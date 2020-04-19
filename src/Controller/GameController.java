ef8db251630e90e4b48c166a6f2e8de5422b002f
duygunuryldz <52247554+duygunuryldz@users.noreply.github.com>
2020-04-19 18:51:44 +0300

    duygu-dev: GameController is modified

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -1,9 +1,10 @@
 package Controller;
 
+import Controller.Fight.FightController;
 import Model.Character;
 import Model.Map;
 import Model.Pet;
-import Model.Room.Room;
+import Model.Room.*;
 
 public class GameController {
     private Map map;
@@ -17,10 +18,36 @@ public class GameController {
         controller = null;
         map = createMap(gameMode);
     }
-
-    public RoomController createController(Room room){
-        return null;
+    public GameController(Character character, int gameMode, Map map){
+        this.character = character;
+        this.gameMode = gameMode;
+        this.map = map;
+        controller = null;
     }
+
+    public void createController(Room room){
+
+        if ( room instanceof UnknownRoom)
+            room = ((UnknownRoom) room).visit();
+
+
+        if(room instanceof TreasureRoom){
+            controller = new TreasureController(character, room);
+        }
+        else if(room instanceof EnemyRoom){
+            controller = new FightController(character, room);
+        }
+        else if(room instanceof MerchantRoom){
+            controller = new MerchantController(character, room);
+        }
+        else if(room instanceof RestRoom){
+            controller = new RestSiteController(character, room);
+        }
+        else if(room instanceof EventRoom){
+            controller = new EventController(character, room);
+        }
+    }
+
 
     public void setActiveRoom(Room room){
 
