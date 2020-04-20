f01b653f636aa755cb67fc3e0ba7b3c4a40e6608
Arda <ardagoktogan@gmail.com>
2020-04-20 23:48:57 +0300

    arda-dev: CardEffectManager implemented

---
+++ src/Model/Cards/BodySlam.java
@@ -0,0 +1,33 @@
+package Model.Cards;
+
+import Model.Card;
+import Model.Effects.Block;
+import Model.Effects.Damage;
+import Model.Effects.Effect;
+import Model.Effects.UpgradeCard;
+import Model.Enemy;
+import Model.Pile;
+
+import java.util.ArrayList;
+
+public class BodySlam extends Card {
+    public BodySlam(String name, String rarity, String type, String color, String description, int energy, boolean upgrade) {
+        super(name, rarity, type, color, description, energy, upgrade);
+    }
+
+
+    /*
+        Deal damage equal to your current Block.
+     */
+
+    public ArrayList<Effect> getEffects(Enemy e, int block){
+        ArrayList<Effect> effects = new ArrayList<Effect>();
+        Effect effect;
+
+        effect = new Damage(block,e);
+        effects.add(effect);
+
+        return effects;
+    }
+
+}
