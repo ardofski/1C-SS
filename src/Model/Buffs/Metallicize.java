2877fdbdbb1c33bad239d4d3ec5200ad0f7694b2
Arda <ardagoktogan@gmail.com>
2020-04-21 22:52:01 +0300

    buff-dev: Mettallicize implemented

--- src/Model/Buffs/Metallicize.java
+++ src/Model/Buffs/Metallicize.java
@@ -1,4 +1,30 @@
 package Model.Buffs;
 
-public class Metallicize {
+import Model.Buff;
+import Model.Effects.Block;
+import Model.Effects.Damage;
+import Model.Effects.Effect;
+import Model.Enemy;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Metallicize extends Buff {
+
+    int x;
+    public Metallicize(String name, int x) {
+        super(name);
+        this.x = x;
+    }
+
+    /*
+         	At the end of your/its turn, gain X Block.
+     */
+
+    public ArrayList<Effect> runNextTurn(Enemy owner ){
+        Block b = new Block( x , owner);
+        ArrayList<Effect> effects = new ArrayList<Effect>();
+        effects.add( b );
+        return effects;
+    }
 }
