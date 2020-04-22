3676c65579e36ca83df447d98baaa57ca3a75ecc
duygunuryldz <duygunuryldz@gmail.com>
2020-04-22 11:15:02 +0300

    getCardObject is implemented.

--- src/DBConnection/CardFactory.java
+++ src/DBConnection/CardFactory.java
@@ -1,6 +1,7 @@
 package DBConnection;
 
 import Model.Card;
+import Model.Cards.*;
 import Model.Pile;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -73,7 +74,31 @@ public class CardFactory {
         String color = (String) card.get("color");
         String description = (String) card.get("description");
         long energy = (long) card.get("energy");
+        return getCardObject(name, rarity, type, color, description, (int)energy, false);
+    }
 
-        return new Card(name, rarity, type, color, description, (int)energy, false);
+    private static Card getCardObject(String name, String rarity, String type, String color, String description, int energy, boolean upgrade){
+        switch (name){
+            case "Anger": return new Anger(name, rarity, type, color, description, energy, upgrade);
+            case "Armaments": return new Armaments(name, rarity, type, color, description, energy, upgrade);
+            case "Bash": return new Bash(name, rarity, type, color, description, energy, upgrade);
+            case "BodySlam": return new BodySlam(name, rarity, type, color, description, energy, upgrade);
+            case "Clash": return new Clash(name, rarity, type, color, description, energy, upgrade);
+            case "Cleave": return new Cleave(name, rarity, type, color, description, energy, upgrade);
+            case "Clothesline": return new Clothesline(name, rarity, type, color, description, energy, upgrade);
+            case "Defend": return new Defend(name, rarity, type, color, description, energy, upgrade);
+            case "Havoc": return new Havoc(name, rarity, type, color, description, energy, upgrade);
+            case "Headbutt": return new Headbutt(name, rarity, type, color, description, energy, upgrade);
+            case "HeavyBlade": return new HeavyBlade(name, rarity, type, color, description, energy, upgrade);
+            case "IronWave": return new IronWave(name, rarity, type, color, description, energy, upgrade);
+            case "PerfectedStrike": return new PerfectedStrike(name, rarity, type, color, description, energy, upgrade);
+            case "PommelStrike": return new PommelStrike(name, rarity, type, color, description, energy, upgrade);
+            case "ShrugItOff": return new ShrugItOff(name, rarity, type, color, description, energy, upgrade);
+            case "Strike": return new Strike(name, rarity, type, color, description, energy, upgrade);
+            case "SwordBoomerang": return new SwordBoomerang(name, rarity, type, color, description, energy, upgrade);
+            case "Thunderclap": return new Thunderclap(name, rarity, type, color, description, energy, upgrade);
+            case "TrueGrit": return new TrueGrit(name, rarity, type, color, description, energy, upgrade);
+        }
+        return new Card(name, rarity, type, color, description, energy, upgrade);
     }
 }
