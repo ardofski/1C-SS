1f2c6149c148dfbcd7aba76eb335ba2793203b0e
Arda <ardagoktogan@gmail.com>
2020-05-13 22:40:02 +0300

    bag of prepration implemented

--- src/Model/Relics/BagOfPreperation.java
+++ src/Model/Relics/BagOfPreperation.java
@@ -1,5 +1,12 @@
 package Model.Relics;
 
+import Controller.Fight.RelicDependencies;
+import Model.Effects.Block;
+import Model.Effects.DrawCard;
+import Model.Effects.Effect;
+
+import java.util.ArrayList;
+
 public class BagOfPreperation extends Relic {
 
     public BagOfPreperation(){
@@ -7,5 +14,13 @@ public class BagOfPreperation extends Relic {
         description = "At the start of each combat, draw 2 additional cards.";
         type = "Common";
         price = 0;
+    }
+
+    @Override
+    public ArrayList<Effect> getBeginingOfFightEffects(RelicDependencies dep) {
+        ArrayList<Effect> effects = new ArrayList<>();
+        effects.add( new DrawCard() );
+        effects.add( new DrawCard() );
+        return effects;
     }
 }
