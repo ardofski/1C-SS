593cd922cea5d120d6cd5d3636a6043be9658345
ErenSenogl <eren.senoglu@ug.bilkent.edu.tr>
2020-05-13 23:51:09 +0300

    Dexterity error fixed.

--- src/Model/Buffs/DexterityDown.java
+++ src/Model/Buffs/DexterityDown.java
@@ -19,6 +19,7 @@ public class DexterityDown extends Buff{
     public ArrayList<Effect> getNextTurnEffects(BuffDependencies dep) {
        Fightable owner = dep.getOwner();
        ApplyBuff buff = new ApplyBuff(new Dexterity("",-x),owner);
+       ApplyBuff buff = new ApplyBuff(new Dexterity(-x),owner);
        ArrayList ddEffect = new ArrayList<Effect>();
        ddEffect.add(buff);
        return ddEffect;
