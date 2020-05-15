7ae29ac8e30ec3c16944a42130cf39f02f0c1a6e
ErenSenogl <eren.senoglu@ug.bilkent.edu.tr>
2020-05-15 17:33:27 +0300

    Event Room implemented.

--- src/GUI/EventScene.java
+++ src/GUI/EventScene.java
@@ -1,10 +1,11 @@
 package GUI;
 
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -13,9 +14,8 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class EventScene {
-    private Scene eventScene;
-    private Stage mainStage;
+public class EventScene extends Parent {
+
     private Pane mainPane;
 
     Rectangle2D screenBounds = Screen.getPrimary().getBounds();
@@ -26,20 +26,30 @@ public class EventScene {
     Image img;
     ImageView imgView;
 
-    public EventScene(){
+    public EventScene(String name,String description,String[] choices, String[] effects){
+        mainPane = new Pane();
+        mainPane.setPrefSize(width, height);
         // Creating and adding event room's unique image
         try {
-            inputStream = Files.newInputStream(Paths.get("resources/images/eventImage.png"));
+            inputStream = Files.newInputStream(Paths.get("resources/images/eventRoomBackground.jpg"));
             img = new Image(inputStream);
             inputStream.close();
             imgView = new ImageView(img);
         }catch (IOException e) {
             e.printStackTrace();
         }
-        mainPane.getChildren().add(imgView);
-        eventScene = new Scene(mainPane,width,height);
-        mainStage = new Stage();
-        mainStage.setScene(eventScene);
+
+        EventImage event = new EventImage(name, description, choices, effects);
+        event.setTranslateX(230);
+        event.setTranslateY(100);
+        mainPane.getChildren().add(event);
+        BackgroundImage mapBG = new BackgroundImage(img,
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                new BackgroundSize(1.0, 1.0, true, true, false, false));
+        Background bg = new Background(mapBG);
+        mainPane.setBackground(bg);
+
+        getChildren().add(mainPane);
     }
 
 
